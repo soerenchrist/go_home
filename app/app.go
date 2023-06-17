e4674e63b9bc2fbfc40b9f701736f93394fdf946
Sören Christ <s.christ@mailbox.org>
2023-06-17 15:12:11 +0200

    Move views to app directory

--- app/app.go
+++ app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"html/template"
+
+	"github.com/foolin/goview"
 	"github.com/foolin/goview/supports/ginview"
 	"github.com/gin-gonic/gin"
 	"github.com/soerenchrist/go_home/db"
@@ -19,11 +22,20 @@ func NewApp(router *gin.Engine, database db.Database) *App {
 }
 
 func (app *App) ServeHtml() {
-	app.router.HTMLRender = ginview.Default()
+	view := ginview.New(goview.Config{
+		Root:         "app/views",
+		Extension:    ".html",
+		Master:       "layouts/master",
+		Partials:     []string{},
+		Funcs:        make(template.FuncMap),
+		DisableCache: false,
+		Delims:       goview.Delims{Left: "{{", Right: "}}"},
+	})
+	app.router.HTMLRender = view
 	app.router.GET("/", app.index)
 	app.router.GET("/devices/:deviceId", app.device)
 	app.router.GET("/devices/:deviceId/sensors/:sensorId", app.sensor)
 	app.router.GET("/devices/:deviceId/commands/:commandId", app.command)
 
-	app.router.Static("/static", "./views/static")
+	app.router.Static("/static", "./app/views/static")
 }
