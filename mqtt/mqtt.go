9a590cae6e53afd4b423c9481b3247e6e9736c69
Sören Christ <s.christ@mailbox.org>
2023-06-19 21:36:46 +0200

    Send mqtt data via http

--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -1,11 +1,15 @@
 package mqtt
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/soerenchrist/go_home/models"
+	"github.com/spf13/viper"
 )
 
 type MqttController struct {
@@ -39,37 +43,53 @@ type Message struct {
 
 type PublishChannel chan Message
 
-func ConnectToBroker(config MqttConfig, publish PublishChannel) error {
+func ConnectToBroker(mqttConf MqttConfig, publish PublishChannel, config *viper.Viper) error {
 	options := mqtt.NewClientOptions()
-	options.AddBroker(fmt.Sprintf("ssl://%s:%d", config.Host, config.Port))
-	options.SetClientID(config.ClientId)
-	options.SetUsername(config.Username)
-	options.SetPassword(config.Password)
+	options.AddBroker(fmt.Sprintf("ssl://%s:%d", mqttConf.Host, mqttConf.Port))
+	options.SetClientID(mqttConf.ClientId)
+	options.SetUsername(mqttConf.Username)
+	options.SetPassword(mqttConf.Password)
 	options.SetDefaultPublishHandler(messagePubHandler)
 	options.OnConnect = connectHandler
 	options.OnConnectionLost = connectLostHandler
 	client := mqtt.NewClient(options)
 
-	log.Printf("Connecting to MQTT broker at %s:%d", config.Host, config.Port)
+	log.Printf("Connecting to MQTT broker at %s:%d", mqttConf.Host, mqttConf.Port)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		return token.Error()
 	}
 	log.Println("Connected to MQTT broker... Listening for publishes")
 
 	go listenForPublishes(client, publish)
-	subscribe(client)
+	subscribe(client, config)
 
 	return nil
 }
 
-func subscribe(client mqtt.Client) {
+func subscribe(client mqtt.Client, config *viper.Viper) {
 	topic := "home/+/+/data"
 	token := client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
 		parts := strings.Split(msg.Topic(), "/")
 		device := parts[1]
 		sensor := parts[2]
 
-		log.Printf("Received message for device %s, sensor %s: %s", device, sensor, msg.Payload())
+		request := models.AddSensorValueRequest{
+			Value: string(msg.Payload()),
+		}
+		body, err := json.Marshal(request)
+		if err != nil {
+			log.Printf("Error while marshalling request: %s", err)
+		}
+
+		host := config.GetString("server.host")
+		port := config.GetInt("server.port")
+
+		url := fmt.Sprintf("http://%s:%d/api/v1/devices/%s/sensors/%s/values", host, port, device, sensor)
+
+		_, err = http.Post(url, "application/json", strings.NewReader(string(body)))
+		if err != nil {
+			log.Printf("Error while sending request to server: %s", err)
+		}
 	})
 	token.Wait()
 	fmt.Printf("Subscribed to topic: %s\n", topic)
